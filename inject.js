225e7804074275473f210cf32abb6dd381c9fa9a
Наташенька ❤️ <evilberry@icloud.com>
2017-04-14 18:34:45 +0700

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,6 +1,6 @@
 (function() {
 	function test(){
-		document.getElementsByName('UN')[0].value = "Орехов"
+		document.getElementsByName('UN')[0].value = "Аплин"
 		document.getElementsByName('PW')[0].value = "1337"
 		document.getElementsByClassName('button-login')[0].click();
 	}
